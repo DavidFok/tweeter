f0ced896cd10280a63bbc8878418e583ee92e126
DavidFok <david_fok@hotmail.com>
2018-02-02 20:19:16 +0000

    added SASS functionality & subsequent filestucture | removed old css files

--- server/index.js
+++ server/index.js
@@ -1,36 +1,37 @@
 "use strict";
 
-// Basic express setup:
+const PORT               = 8080;
+const express            = require("express");
+const bodyParser         = require("body-parser");
+const app                = express();
+const MongoClient        = require("mongodb").MongoClient;
+const uri                = 'mongodb://localhost:27017/tweeter';
+const path               = require('path');
+const nodeSassMiddleware = require('node-sass-middleware');
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-const MongoClient   = require("mongodb").MongoClient;
-
-const uri = 'mongodb://localhost:27017/tweeter';
+console.log(__dirname, "./public");
+console.log(__dirname, "sass");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
+app.use(nodeSassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, '../public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
+
+app.use(express.static('public'));
+
 
 MongoClient.connect(uri, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${uri}`);
     throw err;
   }
-  // Because it exports a function that expects the `db` as a parameter, we can
-  // require it and pass the `db` parameter immediately:
   const DataHelpers = require("./lib/data-helpers.js")(db);
-
-  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-  // so it can define routes that use it to interact with the data layer.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-  // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
 
   app.listen(PORT, () => {
