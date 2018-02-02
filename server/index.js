720dfa28772027b3536e79696f7c94ac6ca0a964
DavidFok <david_fok@hotmail.com>
2018-02-02 22:02:53 +0000

    fixed node-sass middleware functionality to auto update public css when .scss files get updated

--- server/index.js
+++ server/index.js
@@ -9,14 +9,10 @@ const uri                = 'mongodb://localhost:27017/tweeter';
 const path               = require('path');
 const nodeSassMiddleware = require('node-sass-middleware');
 
-console.log(__dirname, "./public");
-console.log(__dirname, "sass");
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(nodeSassMiddleware({
-  src: path.join(__dirname, 'sass'),
-  dest: path.join(__dirname, '../public'),
+  src: path.join(__dirname, 'styles'),
+  dest: path.join(__dirname, 'public'),
   debug: true,
   outputStyle: 'compressed'
 }));
